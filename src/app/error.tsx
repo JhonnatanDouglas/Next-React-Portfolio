b242bdb40e1e29776c0d11ffc3b2f90b81add7df
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2024-02-19 18:39:04 -0300

    feat: cria pagina de 404 e errors para usuario

---
+++ src/app/error.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface iErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+const Error = ({ error, reset }: iErrorProps) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="w-screen h-screen flex">
+      <div className=" m-auto">
+        <div className="h-screen flex flex-col justify-center items-center">
+          <h1 className="lg:text-8xl text-4xl sm:text-6xl font-bold text-gray-800">
+            Ops!
+          </h1>
+          <p className="lg:text-4xl text-xl sm:text-2xl font-medium text-gray-800 my-4">
+            Algo Deu Errado
+          </p>
+          <button
+            onClick={reset}
+            className="w-full px-4 py-2 font-medium text-center text-gray-100 transition-all duration-300 ease-linear bg-gray-700 rounded-md lg:text-lg text-md dark:text-gray-700 dark:hover:text-gray-100 dark:bg-gray-100 dark:hover:bg-purple-600 hover:bg-purple-600"
+          >
+            Tente Novamente
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Error;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import ButtonDefault from "@/components/fragments/button";
+
+const NotFound = () => {
+  return (
+    <main className="w-screen h-screen flex">
+      <div className=" m-auto">
+        <div className="h-screen flex flex-col justify-center items-center">
+          <h1 className="lg:text-8xl text-4xl sm:text-6xl font-bold text-gray-800">
+            404
+          </h1>
+          <p className="lg:text-4xl text-xl sm:text-2xl font-medium text-gray-800 my-4">
+            Página Não Encontrada
+          </p>
+          <ButtonDefault goToPage="/">Voltar ao Início</ButtonDefault>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
