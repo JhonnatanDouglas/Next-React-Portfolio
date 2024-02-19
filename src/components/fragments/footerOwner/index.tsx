e527fe07693df7a865352db5a874d51d22aaa175
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2024-02-19 17:35:04 -0300

    fix: corrije footer'

--- src/components/fragments/footerOwner/index.tsx
+++ src/components/fragments/footerOwner/index.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 const FooterOwner = () => {
   return (
     <div className="flex flex-col items-center gap-1 text-sm font-semibold text-gray-100 md:flex-row sm:flex-row sm:text-center dark:text-gray-100">
-      <div className="flex flex-col-reverse md:gap-1 md:flex-row">
+      <div className="flex flex-col-reverse md:gap-1 md:flex-row text-center">
         <span>
           © 2024
           <strong className="ml-1 font-semibold">
@@ -21,17 +21,11 @@ const FooterOwner = () => {
           </strong>
         </span>
         <span className="hidden md:block"> | </span>
-        <span>Desenvolvido em NextJs</span>
+        <span>Desenvolvido em NextJs com React</span>
       </div>
-      <div className="group-[]: flex flex-row">
+      <div className="group-[]: flex flex-row ml-1">
         <Image
-          className="w-8 mt-1 mr-1 sm:mt-0 md:w-6 animate-pulse"
-          src={nextJsImg}
-          alt="Portfólio | Jhonnatan Douglas Ferreira Araujo - Desenvolvedor Web FullStack, Back-End e Front-End | Kenzie Academy Brasil | Programador Web, NextJs | Desenvolvedor Junior"
-        />
-        <span className="mt-[1.4px]">com React</span>
-        <Image
-          className="w-8 mt-1 ml-1 sm:mt-0 md:w-6 hover:animate-spin group-hover:animate-spin-slow animate-spin-slow"
+          className="w-8 mt-1 sm:mt-0 md:w-6 hover:animate-spin group-hover:animate-spin-slow animate-spin-slow"
           src={reactImg}
           alt="Portfólio | Jhonnatan Douglas Ferreira Araujo - Desenvolvedor Web FullStack, Back-End e Front-End | Kenzie Academy Brasil | Programador Web, React | Desenvolvedor Junior"
         />
