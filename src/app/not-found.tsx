ffab33aac4990e2b9ad2581bfafd81489936116e
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2024-02-20 02:11:22 -0300

    fix: corrige pagina 404 e error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,10 +7,10 @@ const NotFound = () => {
     <main className="w-screen h-screen flex">
       <div className=" m-auto">
         <div className="h-screen flex flex-col justify-center items-center">
-          <h1 className="lg:text-8xl text-4xl sm:text-6xl font-bold text-gray-800">
+          <h1 className="lg:text-8xl text-4xl sm:text-6xl font-bold text-gray-700 dark:text-gray-200">
             404
           </h1>
-          <p className="lg:text-4xl text-xl sm:text-2xl font-medium text-gray-800 my-4">
+          <p className="lg:text-4xl text-xl sm:text-2xl font-medium text-gray-700 dark:text-gray-200 my-4">
             Página Não Encontrada
           </p>
           <ButtonDefault goToPage="/">Voltar ao Início</ButtonDefault>
